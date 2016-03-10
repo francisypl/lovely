0a57d739a2a257edb24d745d6dbaacddbf507eee
francisypl <francisypl@gmail.com>
2016-03-10 12:41:17 -0800

    Facebook SDK set up

---
+++ lovely/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  lovely
+//
+//  Created by Francis Yuen on 3/10/16.
+//  Copyright © 2016 DweebsRUs. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+#endif /* Bridging_Header_h */
