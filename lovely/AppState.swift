17740f83e51bba37e9c979ac2f89cc039e842d8e
francisypl <francisypl@gmail.com>
2016-03-11 17:08:55 -0800

    added max's functions to AppState include function skeletons and properties from the design

--- lovely/AppState.swift
+++ lovely/AppState.swift
@@ -9,9 +9,13 @@
 import Foundation
 
 class AppState {
-    private var loggedIn : Bool
-    private var currentUser : User
-    static var state : AppState?
+    private var loggedIn: Bool
+    private var currentUser: User
+    static var state: AppState?
+    private var publicFeed: [Note]?
+    private var privateFeed: [Note]?
+    private var friendsList: [User]
+    private var pageNumber: Int
     
     static func getInstance() -> AppState {
         if state == nil {
@@ -28,6 +32,52 @@ class AppState {
     
     func isAuthenticated() -> Bool {
         return true
+    }
+    
+    func refreshNotes() {
+    
+    }
+    
+    /**
+     * Gets batch of most recent notes
+     */
+    func getNotes(isPublic: Bool) -> [Note] {
+        let notes = DatabaseWrapper.getNotes(isPublic)
+        
+        //set [Note] to publicFeed/privateFeed
+        
+        return notes
+    }
+    
+    func getCurrentUser() -> User {
+        return User(id: 1, fbId: 1, name: "Name", email: "email", image: UIImage()) //TODO
+    }
+    
+    func suggestRecipientFromFriendList(name : String) -> [User] {
+        return [User(id: 1, fbId: 1, name: "Name", email: "email", image: UIImage())] // TODO
+    }
+    
+    /**
+     * Gets user from database
+     */
+    static func getUserForId(id: Int) -> User {
+        let user = DatabaseWrapper.getUser(id)
+        
+        //join with fb user data
+        
+        return user
+    }
+    
+    /**
+     * Gets batch of notes for appending to bottom of list
+     */
+    private func getMoreNotes(isPublic: Bool) -> [Note] {
+        let lastNote = Note(message: "", recipient: getCurrentUser(), isPublic: true, type: "note", subType: NoteSubType.Love) //TODO - this is just a random note
+        
+        let notes = DatabaseWrapper.getNotes(lastNote, isPublic: isPublic)
+        //append [Note] to publicFeed/privateFeed
+        
+        return notes
     }
 
 }
